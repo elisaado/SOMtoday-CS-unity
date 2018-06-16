88ce13760f2eab782c28ec3918aa5be31a493b2d
elisaado <elimalkosaado@gmail.com>
2018-06-16 14:58:14 +0200

    Create Som.cs

---
+++ Som.cs
@@ -0,0 +1,11 @@
+/*
+* By Eli Saado, licensed under the MIT license, found here: https://github.com/elisaado/SOMtoday-CS-unity/blob/master/LICENSE
+* This library can only be used in Unity, as it uses Unity specific HTTP thingies.
+*
+* Using this library, you'll be able to:
+*   [ ] Log In (fetch the tokens)
+*   [ ] Refresh the tokens
+*   [ ] Fetch the pupil associated with the token(s)
+*   [ ] Fetch grades from that pupil
+*/
+
